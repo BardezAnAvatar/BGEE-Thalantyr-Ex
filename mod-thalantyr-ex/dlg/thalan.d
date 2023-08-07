efee28bd5811f6abb7b0499c1506d69232559ed8
Erik Frantz <bardez@bardez\.net>
2023-08-07 02:23:56 -0500

    Shandalar's cloak text was too large; use Multisay instead

--- mod-thalantyr-ex/dlg/thalan.d
+++ mod-thalantyr-ex/dlg/thalan.d
@@ -3,29 +3,31 @@
 APPEND ~THALAN~
   IF ~~ THEN BEGIN bardez_thalantyr_shandalar_cloak // from: ThalantyrCraftingState
     SAY @1002 /* ~Oh, that's rich [...]~ */
+      =
+      @1003 /* [...] I'll offer you this: [...]*/
     IF ~~ THEN REPLY @9000 /* ~I don't need it right now. Maybe I have something else you could alter?~ */ 
       GOTO %ThalantyrCraftingState%
     IF 
-  ~PartyHasItem("CLCK22")
-  PartyHasItem("MISC2H")
-  NumItemsPartyGT("SCRL05",1)
-  NumItemsPartyGT("SCRL06",1)
-  PartyGoldGT(9999)~
+~PartyHasItem("CLCK22")
+PartyHasItem("MISC2H")
+NumItemsPartyGT("SCRL05",1)
+NumItemsPartyGT("SCRL06",1)
+PartyGoldGT(9999)~
       THEN
-          REPLY @1003 /* ~Yes, restore it for me.~ */
+          REPLY @1004 /* ~Yes, restore it for me.~ */
           DO
-  ~TakePartyGold(10000)
-  DestroyGold(10000)
-  TakePartyItemNum("CLCK22",1)
-  DestroyItem("CLCK22")
-  TakePartyItemNum("MISC2H",1)
-  DestroyItem("MISC2H")
-  TakePartyItemNum("SCRL05",2)
-  DestroyItem("SCRL05")
-  TakePartyItemNum("SCRL06",2)
-  DestroyItem("SCRL06")
-  GiveItemCreate("BZCLK1",Player1,1,1,1)
-  CreateVisualEffect("spcrtwpn",[330.230])~
+~TakePartyGold(10000)
+DestroyGold(10000)
+TakePartyItemNum("CLCK22",1)
+DestroyItem("CLCK22")
+TakePartyItemNum("MISC2H",1)
+DestroyItem("MISC2H")
+TakePartyItemNum("SCRL05",2)
+DestroyItem("SCRL05")
+TakePartyItemNum("SCRL06",2)
+DestroyItem("SCRL06")
+GiveItemCreate("BZCLK1",Player1,1,1,1)
+CreateVisualEffect("spcrtwpn",[330.230])~
           EXIT
   END
 END
