73e981189e3652ae69153dd9f71714d5c7fa1d6a
Erik Frantz <bardez@bardez.net>
2023-08-28 17:24:18 -0500

    Taking a stab at abstracting actions into a TRA file. Not for languages, just for string replacements

--- mod-thalantyr-ex/dlg/thalan.d
+++ mod-thalantyr-ex/dlg/thalan.d
@@ -7,57 +7,20 @@ APPEND ~THALAN~
       @1003 /* [...] I'll offer you this: [...]*/
     IF ~~ THEN REPLY @9000 /* ~I don't need it right now. Maybe I have something else you could alter?~ */ 
       GOTO %ThalantyrCraftingState%
-    IF 
-~PartyHasItem("CLCK22")
-PartyHasItem("MISC2H")
-NumItemsPartyGT("SCRL05",1)
-NumItemsPartyGT("SCRL06",1)
-PartyGoldGT(9999)~
+    IF @30000 //requirement recipe
       THEN
           REPLY @1004 /* ~Yes, restore it for me.~ */
-          DO
-~TakePartyGold(10000)
-DestroyGold(10000)
-TakePartyItemNum("CLCK22",1)
-DestroyItem("CLCK22")
-TakePartyItemNum("MISC2H",1)
-DestroyItem("MISC2H")
-TakePartyItemNum("SCRL05",2)
-DestroyItem("SCRL05")
-TakePartyItemNum("SCRL06",2)
-DestroyItem("SCRL06")
-GiveItemCreate("BZCLK1",Player1,1,1,1)
-CreateVisualEffect("spcrtwpn",[330.230])~
+          DO @30001 //action
           EXIT
   END
   IF ~~ THEN BEGIN bardez_thalantyr_varscona3 // from: ThalantyrCraftingState
     SAY @1101 /* ~Ah, Varscona [...]~ */
     IF ~~ THEN REPLY @9000 /* ~I don't need it right now. Maybe I have something else you could alter?~ */ 
       GOTO %ThalantyrCraftingState%
-    IF 
-~PartyHasItem("SW1H06")
-PartyHasItem("MISC43")
-PartyHasItem("SCRL06")
-PartyHasItem("SCRL1Q")
-PartyHasItem("SCRL2F")
-PartyGoldGT(5999)~
+    IF @30010 //requirement
       THEN
           REPLY @1102 /* ~Yes, restore it for me.~ */
-          DO
-~TakePartyGold(6000)
-DestroyGold(6000)
-TakePartyItemNum("SW1H06",1)
-DestroyItem("SW1H06")
-TakePartyItemNum("MISC43",1)
-DestroyItem("MISC43")
-TakePartyItemNum("SCRL06",1)
-DestroyItem("SCRL06")
-TakePartyItemNum("SCRL1Q",1)
-DestroyItem("SCRL1Q")
-TakePartyItemNum("SCRL2F",1)
-DestroyItem("SCRL2F")
-GiveItemCreate("BZS1H1C",Player1,1,1,1)
-CreateVisualEffect("spcrtwpn",[330.230])~
+          DO @30011 //action
           EXIT
   END
   IF ~~ THEN BEGIN bardez_thalantyr_koveras_uncurse // from: ThalantyrCraftingState
@@ -65,27 +28,10 @@ CreateVisualEffect("spcrtwpn",[330.230])~
     = @1202 /* Ah. This is a particularly nasty curse. Potent, too. [...] */
     IF ~~ THEN REPLY @9000 /* ~I don't need it right now. Maybe I have something else you could alter?~ */ 
       GOTO %ThalantyrCraftingState%
-    IF 
-~PartyHasItem("BZRNG1")
-NumItemsPartyGT("SCRL5G",2)
-NumItemsPartyGT("POTN28",1)
-NumItemsPartyGT("MISC34",2)
-PartyGoldGT(2999)~
+    IF @30020 //requirements
       THEN
           REPLY @1203 /* ~Yes, please remove the curse for me.~ */
-          DO
-~TakePartyGold(3000)
-DestroyGold(3000)
-TakePartyItemNum("MISC34",3)
-DestroyItem("MISC34")
-TakePartyItemNum("POTN28",2)
-DestroyItem("POTN28")
-TakePartyItemNum("SCRL5G",3)
-DestroyItem("SCRL5G")
-TakePartyItemNum("BZRNG1",1)
-DestroyItem("BZRNG1")
-GiveItemCreate("RING25",Player1,1,1,1)
-CreateVisualEffect("spcrtwpn",[330.230])~
+          DO @30021 //action
           EXIT
   END
   IF ~~ THEN BEGIN bardez_thalantyr_koveras_ring_2 // from: ThalantyrCraftingState
